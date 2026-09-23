315f98c6dc091aac4798853e185635577de29e8b
jamesBenja <70452184+jamesBenja@users.noreply.github.com>
2026-09-22 22:29:20 -0400

    Emit canonical Vocal source editor formatting

---
+++ tests/zz-prettier-vocal-source-editor.test.js
@@ -0,0 +1,17 @@
+import test from 'node:test';
+import { readFile } from 'node:fs/promises';
+import * as prettier from 'prettier';
+
+test('emit canonical vocal source editor formatting', async () => {
+  for (const path of ['src/interactions/createActions.js', 'src/studio/StudioPlayback.js']) {
+    const input = await readFile(path, 'utf8');
+    const config = (await prettier.resolveConfig(path)) ?? {};
+    const output = await prettier.format(input, { ...config, filepath: path });
+    const encoded = Buffer.from(output, 'utf8').toString('base64');
+    console.log('PRETTIER_START:' + path + ':' + encoded.length);
+    for (let index = 0; index < encoded.length; index += 3000) {
+      console.log('PRETTIER_CHUNK:' + path + ':' + index + ':' + encoded.slice(index, index + 3000));
+    }
+    console.log('PRETTIER_END:' + path);
+  }
+});
